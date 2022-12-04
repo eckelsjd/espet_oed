Log trick to fix underflow in nmc

src/nmc.py
@@ -56,11 +56,19 @@ def eig_nmc(x_loc, theta_sampler, model, N=100, M=100, noise_cov=1.0, reuse_samp
     def parallel_func(idx, y_i, g_theta_i, evidence):
         y_curr = y_i[idx, np.newaxis, :, :, :]                          # (1, Nr, Nx, y_dim)
         if reuse_samples:
-            evidence[idx, :, :] = np.mean(batch_normal_pdf(y_curr, g_theta_i, noise_cov), axis=0)
+            log_like = batch_normal_pdf(y_curr, g_theta_i, noise_cov, logpdf=True)  # (N, Nr, Nx)
+            max_log_like = np.expand_dims(np.max(log_like, axis=0), axis=0)  # (1, Nr, Nx)
+            evidence[idx, :, :] = -np.log(N) + np.squeeze(max_log_like, axis=0) + \
+                                  np.log(np.sum(np.exp(log_like - max_log_like), axis=0))
+            # evidence[idx, :, :] = np.mean(batch_normal_pdf(y_curr, g_theta_i, noise_cov), axis=0)
         else:
             theta_j = theta_sampler((M, Nr, Nx)).astype(np.float32)     # (M, Nr, Nx, theta_dim)
             g_theta_j = model(x_loc, theta_j)                           # (M, Nr, Nx, y_dim)
-            evidence[idx, :, :] = np.mean(batch_normal_pdf(y_curr, g_theta_j, noise_cov), axis=0)
+            log_like = batch_normal_pdf(y_curr, g_theta_j, noise_cov, logpdf=True)  # (M, Nr, Nx)
+            max_log_like = np.expand_dims(np.max(log_like, axis=0), axis=0)  # (1, Nr, bs)
+            evidence[idx, :, :] = -np.log(M) + np.squeeze(max_log_like, axis=0) + \
+                                  np.log(np.sum(np.exp(log_like - max_log_like), axis=0))
+            # evidence[idx, :, :] = np.mean(batch_normal_pdf(y_curr, g_theta_j, noise_cov), axis=0)
 
         if ((idx + 1) % 100) == 0 and n_jobs == 1:
             logging.info(f'Samples processed: {idx + 1} out of {N}')
@@ -72,10 +80,13 @@ def eig_nmc(x_loc, theta_sampler, model, N=100, M=100, noise_cov=1.0, reuse_samp
         ppool(delayed(parallel_func)(idx, y_i, g_theta_i, evidence) for idx in range(N))
 
     # Compute likelihood
-    likelihood = batch_normal_pdf(y_i, g_theta_i, noise_cov)        # (N, Nr, Nx)
+    likelihood = batch_normal_pdf(y_i, g_theta_i, noise_cov, logpdf=True)        # (N, Nr, Nx)
 
     # Expected information gain
-    eig = np.mean(np.log(likelihood) - np.log(evidence), axis=0)    # (Nr, Nx)
+    likelihood = np.nan_to_num(likelihood, neginf=np.nan, posinf=np.nan)
+    evidence = np.nan_to_num(evidence, neginf=np.nan, posinf=np.nan)
+    eig = np.nanmean(likelihood - evidence, axis=0)  # (Nr, Nx)
+    # eig = np.mean(np.log(likelihood) - np.log(evidence), axis=0)    # (Nr, Nx)
 
     # Clean up
     del theta_i
@@ -163,24 +174,42 @@ def eig_nmc_pm(x_loc, theta_sampler, eta_sampler, model, N=100, M1=100, M2=100, 
             theta_curr = theta_i[idx, np.newaxis, :, b_slice, :]                       # (1, Nr, bs, theta_dim)
             if reuse_samples:
                 # Compute evidence
-                evidence[idx, :, b_slice] = np.mean(batch_normal_pdf(y_curr, g_theta_i[:, :, b_slice, :], noise_cov),
-                                                    axis=0)
+                log_like = batch_normal_pdf(y_curr, g_theta_i[:, :, b_slice, :], noise_cov, logpdf=True)  # (M1, Nr, bs)
+                max_log_like = np.expand_dims(np.max(log_like, axis=0), axis=0)  # (1, Nr, bs)
+                evidence[idx, :, b_slice] = -np.log(N) + np.squeeze(max_log_like, axis=0) + \
+                                            np.log(np.sum(np.exp(log_like - max_log_like), axis=0))
+                # evidence[idx, :, b_slice] = np.mean(batch_normal_pdf(y_curr, g_theta_i[:, :, b_slice, :], noise_cov),
+                #                                     axis=0)
+
                 # Compute likelihood
                 g_theta_k = model(x_loc[b_slice, :], theta_curr, eta_i[:, :, b_slice, :])  # (N, Nr, bs, y_dim)
-                likelihood[idx, :, b_slice] = np.mean(batch_normal_pdf(y_curr, g_theta_k, noise_cov), axis=0)
+                log_like = batch_normal_pdf(y_curr, g_theta_k, noise_cov, logpdf=True)
+                max_log_like = np.expand_dims(np.max(log_like, axis=0), axis=0)
+                likelihood[idx, :, b_slice] = -np.log(N) + np.squeeze(max_log_like, axis=0) + \
+                                              np.log(np.sum(np.exp(log_like - max_log_like), axis=0))
+                # likelihood[idx, :, b_slice] = np.mean(batch_normal_pdf(y_curr, g_theta_k, noise_cov), axis=0)
             else:
                 # Compute evidence
                 eta_j = eta_sampler((M1, Nr, batch_size)).astype(np.float32)            # (M1, Nr, bs, eta_dim)
                 theta_j = theta_sampler((M1, Nr, batch_size)).astype(np.float32)        # (M1, Nr, bs, theta_dim)
                 g_theta_j = model(x_loc[b_slice, :], theta_j, eta_j)                    # (M1, Nr, bs, y_dim)
-                evidence[idx, :, b_slice] = np.mean(batch_normal_pdf(y_curr, g_theta_j, noise_cov), axis=0)
+                log_like = batch_normal_pdf(y_curr, g_theta_j, noise_cov, logpdf=True)  # (M1, Nr, bs)
+                max_log_like = np.expand_dims(np.max(log_like, axis=0), axis=0)         # (1, Nr, bs)
+                evidence[idx, :, b_slice] = -np.log(M1) + np.squeeze(max_log_like, axis=0) + \
+                                            np.log(np.sum(np.exp(log_like - max_log_like), axis=0))
+                # evidence[idx, :, b_slice] = np.mean(batch_normal_pdf(y_curr, g_theta_j, noise_cov), axis=0)
 
                 # Compute likelihood
                 eta_k = eta_sampler((M2, Nr, batch_size)).astype(np.float32)            # (M2, Nr, bs, eta_dim)
                 g_theta_k = model(x_loc[b_slice, :], theta_curr, eta_k)                 # (M2, Nr, bs, y_dim)
-                likelihood[idx, :, b_slice] = np.mean(batch_normal_pdf(y_curr, g_theta_k, noise_cov), axis=0)
+                log_like = batch_normal_pdf(y_curr, g_theta_k, noise_cov, logpdf=True)
+                max_log_like = np.expand_dims(np.max(log_like, axis=0), axis=0)
+                likelihood[idx, :, b_slice] = -np.log(M2) + np.squeeze(max_log_like, axis=0) + \
+                                              np.log(np.sum(np.exp(log_like - max_log_like), axis=0))
+                # likelihood[idx, :, b_slice] = np.mean(batch_normal_pdf(y_curr, g_theta_k, noise_cov), axis=0)
 
-        print(f'Parallel idx {idx}: {time.time()-t1:.02} s')
+        if idx % 500 == 0:
+            print(f'Parallel idx {idx}: {time.time()-t1:.02} s')
 
     # Compute evidence p(y|d) and likelihood p(y|theta, d)
     if ppool is None:
@@ -190,7 +219,10 @@ def eig_nmc_pm(x_loc, theta_sampler, eta_sampler, model, N=100, M1=100, M2=100, 
         ppool(delayed(parallel_func)(idx, theta_i, eta_i, y_i, g_theta_i, likelihood, evidence) for idx in range(N))
 
     # Expected information gain
-    eig = np.mean(np.log(likelihood) - np.log(evidence), axis=0)    # (Nr, Nx)
+    likelihood = np.nan_to_num(likelihood, neginf=np.nan, posinf=np.nan)
+    evidence = np.nan_to_num(evidence, neginf=np.nan, posinf=np.nan)
+    eig = np.nanmean(likelihood - evidence, axis=0)  # (Nr, Nx)
+    # eig = np.mean(np.log(likelihood) - np.log(evidence), axis=0)    # (Nr, Nx)
 
     # Clean up
     del theta_i

test/test_mse.py
@@ -95,19 +95,18 @@ def test_nmc(model='linear'):
         d = np.linspace(800, 1845, Nx)
         theta_sampler, eta_sampler = electrospray_samplers()
         model_func = lambda *args, **kwargs: electrospray_current_model(*args, **kwargs, gpu=True)
-        N = 10  # 10000
-        M = 100  # 500
-        Nr = 4  # 20
-        bs = 25  # 50
+        N = 2000  # 10000
+        M = 1500  # 500
+        Nr = 20  # 20
+        bs = 1  # 50
         exp_data = np.loadtxt('../data/training_data.txt', dtype=np.float32, delimiter='\t')
         gamma = np.mean(exp_data[2, :])
-        print('Starting main electrospray ground truth')
         t1 = time.time()
         eig_estimate = np.zeros((Nr, Nx))
-        with Parallel(n_jobs=2, verbose=9) as ppool:
+        with Parallel(n_jobs=-1, verbose=9) as ppool:
             for i in range(Nr):
                 eig = eig_nmc_pm(d, theta_sampler, eta_sampler, model_func, N=N, M1=M, M2=M, noise_cov=gamma,
-                                 reuse_samples=False, n_jobs=2, batch_size=bs, replicates=1, ppool=ppool)
+                                 reuse_samples=False, n_jobs=-1, batch_size=bs, replicates=1, ppool=ppool)
                 eig_estimate[i, :] = np.squeeze(eig, axis=0)
         t2 = time.time()
         print(f'Total time for N={N} M={M} Nr={Nr} bs={bs}: {t2-t1:.02} s')
@@ -153,12 +152,11 @@ def test_nmc(model='linear'):
             # Run NMC estimator
             eig_estimate = np.zeros((N_MC, Nx))
             with Parallel(n_jobs=-1, verbose=9) as ppool:
-                for i in range(N_MC):
+                for k in range(N_MC):
                     eig = eig_nmc_pm(d, theta_sampler, eta_sampler, model_func, N=N, M1=M, M2=M, noise_cov=gamma,
-                                     reuse_samples=False, n_jobs=-1, batch_size=5, replicates=1, ppool=ppool)
-                    eig_estimate[i, :] = np.squeeze(eig, axis=0)
-            # eig_estimate = eig_nmc_pm(d, theta_sampler, eta_sampler, model_func, N=N, M1=M, M2=M,
-            #                           noise_cov=gamma, reuse_samples=False, n_jobs=-1, replicates=N_MC)  # (N_MC, Nx)
+                                     reuse_samples=False, n_jobs=-1, batch_size=1, replicates=1, ppool=ppool)
+                    eig_estimate[k, :] = np.squeeze(eig, axis=0)
+
             # Filter arithmetic underflow
             eig_store[i, j, :, :] = np.nan_to_num(eig_estimate, posinf=np.nan, neginf=np.nan)
             mse_store[i, j, :] = np.nanmean((eig_store[i, j, :, :] - eig_truth) ** 2, axis=-1)  # (N_MC,)
@@ -250,28 +248,31 @@ def plot_nmc(model='linear', estimator='nmc'):
     for i, nm_ratio in enumerate(N_to_M):
         # Plot bias
         bias_mean = np.nanmean(np.abs(bias), axis=-1)   # (N_est, N_cost)
-        nx_var = np.nanvar(np.abs(bias), axis=-1)       # (N_est, N_cost)
-        std_err = np.sqrt(nx_var / Nx)
+        axs[0].plot(cost[i, :], bias_mean[i, :], linestyle='-', markersize=4, linewidth=1.3)
         axs[0].set_ylabel(r'Estimator bias')
-        axs[0].errorbar(cost[i, :], bias_mean[i, :], yerr=1.96 * std_err[i, :], linestyle='-',
-                        markersize=0, linewidth=1.3, capsize=2)
-        # axs[0].plot(cost[i, :], np.abs(bias_mean[i, :]), linestyle='-', markersize=1, linewidth=1.3)
+        # nx_var = np.nanvar(np.abs(bias), axis=-1)       # (N_est, N_cost)
+        # std_err = np.sqrt(nx_var / Nx)
+        # axs[0].errorbar(cost[i, :], bias_mean[i, :], yerr=1.96 * std_err[i, :], linestyle='-',
+        #                 markersize=0, linewidth=1.3, capsize=2)
 
         # Plot variance
         label = f"{int(nm_ratio)}:1" if nm_ratio >= 1 else f"1:{int(1 / nm_ratio)}"
         var_mean = np.nanmean(var, axis=-1)     # (N_est, N_cost)
-        nx_var = np.nanvar(var, axis=-1)        # (N_est, N_cost)
-        std_err = np.sqrt(nx_var / Nx)
+        axs[1].plot(cost[i, :], var_mean[i, :], linestyle='-', markersize=4, linewidth=1.3)
         axs[1].set_ylabel(r'Estimator variance')
-        axs[1].errorbar(cost[i, :], var_mean[i, :], yerr=1.96 * std_err[i, :], linestyle='-',
-                        markersize=0, linewidth=1.3, capsize=2, label=label)
+        # nx_var = np.nanvar(var, axis=-1)        # (N_est, N_cost)
+        # std_err = np.sqrt(nx_var / Nx)
+        # axs[1].errorbar(cost[i, :], var_mean[i, :], yerr=1.96 * std_err[i, :], linestyle='-',
+        #                 markersize=0, linewidth=1.3, capsize=2, label=label)
+
         # Plot MSE
         mse_mean = np.nanmean(mse, axis=-1)     # (N_est, N_cost)
-        nx_var = np.nanvar(mse, axis=-1)        # (N_est, N_cost)
-        std_err = np.sqrt(nx_var / Nx)
+        axs[2].plot(cost[i, :], mse_mean[i, :], linestyle='-', markersize=4, linewidth=1.3)
         axs[2].set_ylabel(r'Estimator MSE')
-        axs[2].errorbar(cost[i, :], mse_mean[i, :], yerr=1.96 * std_err[i, :], linestyle='-',
-                        markersize=0, linewidth=1.3, capsize=2)
+        # nx_var = np.nanvar(mse, axis=-1)        # (N_est, N_cost)
+        # std_err = np.sqrt(nx_var / Nx)
+        # axs[2].errorbar(cost[i, :], mse_mean[i, :], yerr=1.96 * std_err[i, :], linestyle='-',
+        #                 markersize=0, linewidth=1.3, capsize=2)
 
     axs[0].set_ylim(bottom=3e-5)
     fig.set_size_inches(9, 3.5)

test/test_oed.py
@@ -15,7 +15,7 @@ from src.utils import model_1d_batch, ax_default, linear_eig, electrospray_sampl
 def test_linear_gaussian_model(estimator='nmc'):
     # Linear gaussian model example
     N = 500
-    M = 50
+    M = 500
     Nx = 50
     Nr = 20
     noise_var = 0.01
@@ -41,11 +41,11 @@ def test_linear_gaussian_model(estimator='nmc'):
 
     # Compute estimator EIG
     if estimator == 'nmc':
-        # eig_estimate = eig_nmc_pm(x_loc, theta_sampler, eta_sampler, linear_gaussian_model, N=N, M1=M, M2=M,
-        #                           noise_cov=noise_cov, reuse_samples=False, n_jobs=-1, batch_size=-1, replicates=Nr)
-        theta_sampler = lambda shape: np.random.randn(*shape, 2) * np.sqrt(prior_cov) + prior_mean
-        eig_estimate = eig_nmc(x_loc, theta_sampler, linear_gaussian_model, N=N, M=M, replicates=Nr,
-                               noise_cov=noise_cov, reuse_samples=False, n_jobs=-1)
+        eig_estimate = eig_nmc_pm(x_loc, theta_sampler, eta_sampler, linear_gaussian_model, N=N, M1=M, M2=M,
+                                  noise_cov=noise_cov, reuse_samples=False, n_jobs=-1, batch_size=-1, replicates=Nr)
+        # theta_sampler = lambda shape: np.random.randn(*shape, 2) * np.sqrt(prior_cov) + prior_mean
+        # eig_estimate = eig_nmc(x_loc, theta_sampler, linear_gaussian_model, N=N, M=M, replicates=Nr,
+        #                        noise_cov=noise_cov, reuse_samples=False, n_jobs=-1)
     elif estimator == 'mcla':
         pass
         # Marginal
@@ -60,22 +60,22 @@ def test_linear_gaussian_model(estimator='nmc'):
         #                         noise_cov=noise_cov, replicates=Nr, n_jobs=-1, batch_size=-1)
 
     # Compute percentiles over replicates
-    # eig_lb = np.nanpercentile(eig_estimate, 5, axis=0)
-    # eig_med = np.nanpercentile(eig_estimate, 50, axis=0)
-    # eig_ub = np.nanpercentile(eig_estimate, 95, axis=0)
+    eig_lb = np.nanpercentile(eig_estimate, 5, axis=0)
+    eig_med = np.nanpercentile(eig_estimate, 50, axis=0)
+    eig_ub = np.nanpercentile(eig_estimate, 95, axis=0)
 
     fig, ax = plt.subplots()
     ax.plot(d, eig_truth_marg, '--k', label=r'Marginal $p(\theta)$')
     ax.plot(d, eig_truth_joint, '-k', label=r'Joint $p(\theta, \phi)$')
-    # ax.plot(d, eig_med, '-r', label=r'Estimator')
-    # ax.fill_between(d, eig_lb, eig_ub, alpha=0.3, edgecolor=(0.5, 0.5, 0.5), facecolor='red')
+    ax.plot(d, eig_med, '-r', label=r'Estimator')
+    ax.fill_between(d, eig_lb, eig_ub, alpha=0.3, edgecolor=(0.5, 0.5, 0.5), facecolor='red')
     ax_default(ax, xlabel='Operating condition $d$', ylabel='Expected information gain', legend=True)
     ax.set_xlim(left=0, right=1)
     ax.set_ylim(bottom=-0.01)
     fig.set_size_inches(4.8, 3.6)
     plt.tight_layout()
     plt.show()
-    fig.savefig(str(Path('../results/figs') / f'linear-eig.png'), dpi=300, format='png')
+    # fig.savefig(str(Path('../results/figs') / f'linear-eig.png'), dpi=300, format='png')
 
 
 def test_1d_nonlinear_model():
@@ -83,30 +83,30 @@ def test_1d_nonlinear_model():
     prior_cov = 0.4**2
     # theta_sampler = lambda shape: np.random.randn(*shape, 1)*np.sqrt(prior_cov) + prior_mean
     theta_sampler = lambda shape: np.random.rand(*shape, 1)
-    N = 2000
-    M = 1000
+    N = 1000
+    M = 500
     Nx = 50
     x_loc = np.linspace(0, 1, Nx).reshape((Nx, 1))
     d = np.squeeze(x_loc)
     var = 1e-4
-    eig = eig_nmc(x_loc, theta_sampler, nonlinear_model, N=N, M=M, replicates=10, noise_cov=var, reuse_samples=False,
+    eig = eig_nmc(x_loc, theta_sampler, nonlinear_model, N=N, M=M, replicates=50, noise_cov=var, reuse_samples=False,
                   n_jobs=-1)
     eig_lb = np.nanpercentile(eig, 5, axis=0)
     eig_med = np.nanpercentile(eig, 50, axis=0)
     eig_ub = np.nanpercentile(eig, 95, axis=0)
 
     # MCLA estimator
-    eig_est = eig_mcla(x_loc, theta_sampler, nonlinear_model, prior_mean, prior_cov, N=N, Ne=10, noise_cov=var,
-                       replicates=10, n_jobs=-1, batch_size=-1)
-    eig_est_lb = np.nanpercentile(eig_est, 5, axis=0)
-    eig_est_med = np.nanpercentile(eig_est, 50, axis=0)
-    eig_est_ub = np.nanpercentile(eig_est, 95, axis=0)
+    # eig_est = eig_mcla(x_loc, theta_sampler, nonlinear_model, prior_mean, prior_cov, N=N, Ne=10, noise_cov=var,
+    #                    replicates=10, n_jobs=-1, batch_size=-1)
+    # eig_est_lb = np.nanpercentile(eig_est, 5, axis=0)
+    # eig_est_med = np.nanpercentile(eig_est, 50, axis=0)
+    # eig_est_ub = np.nanpercentile(eig_est, 95, axis=0)
 
     fig, ax = plt.subplots()
     ax.plot(d, eig_med, '-k', label=r'NMC')
     ax.fill_between(d, eig_lb, eig_ub, alpha=0.3, edgecolor=(0.5, 0.5, 0.5), facecolor='gray')
-    ax.plot(d, eig_est_med, '-r', label=r'MCLA')
-    ax.fill_between(d, eig_est_lb, eig_est_ub, alpha=0.3, edgecolor=(0.5, 0.5, 0.5), facecolor='red')
+    # ax.plot(d, eig_est_med, '-r', label=r'MCLA')
+    # ax.fill_between(d, eig_est_lb, eig_est_ub, alpha=0.3, edgecolor=(0.5, 0.5, 0.5), facecolor='red')
     ax_default(ax, xlabel='Operating condition $d$', ylabel='Expected information gain', legend=True)
     ax.set_xlim(left=0, right=1)
     fig.set_size_inches(4.8, 3.6)
@@ -150,27 +150,33 @@ def test_array_current_model(dim=1):
 
     # Experimental data
     exp_data = np.loadtxt('../data/training_data.txt', dtype=np.float32, delimiter='\t')
-    var = 2 * np.max(exp_data[2, :])
+    var = np.mean(exp_data[2, :])
 
     # Set sample sizes
-    N = 500
-    Nx = 40
-    M1 = 300
-    M2 = 300
+    N = 100
+    Nx = 50
+    M = 10
     n_jobs = -1
-    bs = -1
+    bs = 10
+    Nr = 10
 
     if dim == 1:
-        x_loc = np.linspace(800, 1840, Nx).reshape((Nx, 1))
-        eig = eig_nmc_pm(x_loc, theta_sampler, eta_sampler, electrospray_current_model, N=N, M1=M1, M2=M2,
-                         noise_cov=var, reuse_samples=False, n_jobs=n_jobs, batch_size=bs)
-        plt.figure()
-        plt.plot(x_loc, eig, '-k')
-        plt.xlabel('Voltage [V]')
-        plt.ylabel('Expected information gain')
-        plt.show()
+        x_loc = np.linspace(800, 1845, Nx).reshape((Nx, 1))
+        d = np.squeeze(x_loc)
+        eig_estimate = eig_nmc_pm(x_loc, theta_sampler, eta_sampler, electrospray_current_model, N=N, M1=M, M2=M,
+                         noise_cov=var, reuse_samples=False, n_jobs=n_jobs, batch_size=bs, replicates=Nr)
+        eig_lb_pm = np.nanpercentile(eig_estimate, 5, axis=0)
+        eig_med_pm = np.nanpercentile(eig_estimate, 50, axis=0)
+        eig_ub_pm = np.nanpercentile(eig_estimate, 95, axis=0)
 
-        return eig
+        fig, ax = plt.subplots()
+        ax.plot(d, eig_med_pm, '-r', label=r'Marginal p($\theta$)')
+        ax.fill_between(d, eig_lb_pm, eig_ub_pm, alpha=0.3, edgecolor=(0.5, 0.5, 0.5), facecolor='red')
+        ax.set_ylim(bottom=-0.01)
+        ax_default(ax, xlabel='Operating condition $d$', ylabel='Expected information gain')
+        fig.set_size_inches(4.8, 3.6)
+        fig.tight_layout()
+        plt.show()
 
     if dim == 2:
         Ngrid = [Nx, Nx]
@@ -261,8 +267,8 @@ def test_lg():
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    # eig = test_array_current_model(dim=1)
+    # test_linear_gaussian_model(estimator='nmc')
     # test_custom_nonlinear()
     # test_1d_nonlinear_model()
-    # test_linear_gaussian_model(estimator='mcla')
-    test_lg()
+    test_array_current_model(dim=1)
+    # test_lg()